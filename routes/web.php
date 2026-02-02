d01560ce6bcebffe58e83d6bf0c49a60308d7971
kevin <kevinkalenga10@gmail.com>
2026-02-02 22:41:25 +0100

    Admin Logout Option

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,10 @@ Route::get('/dashboard', function () {
 
 Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
+
+ Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 
 // Vendor Dashboard
 Route::middleware(['auth', 'role:vendor'])->group(function(){
