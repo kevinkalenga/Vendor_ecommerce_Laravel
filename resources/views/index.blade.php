9be8b130c72285a302188c8613ef563bebc355ae
kevin <kevinkalenga10@gmail.com>
2026-02-06 14:54:41 +0100

    User Profile, Password Change Part1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,10 @@
                                             <a class="nav-link" id="account-detail-tab" data-bs-toggle="tab" href="#account-detail" role="tab" aria-controls="account-detail" aria-selected="true"><i class="fi-rs-user mr-10"></i>Account details</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" href="page-login.html"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
+                                            <a class="nav-link" id="change-password-tab" data-bs-toggle="tab" href="#change-password" role="tab" aria-controls="change-password" aria-selected="true"><i class="fi-rs-user mr-10"></i>Change Password</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{route('user.logout')}}"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -198,6 +201,63 @@
                                                             <label><span class="required">*</span></label>
                                                            <img  id="showImage"  src="{{(!empty($userData->photo)) ? url('upload/user_images/'.$userData->photo) : url('upload/default_avatar.jpg')}}" alt="User" class="rounded-circle p-1 bg-primary" width="110">
                                                         </div>
+                                                        
+                                                        <div class="col-md-12">
+                                                            <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit" value="Submit">Save Change</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Change Password -->
+                                    <div class="tab-pane fade" id="change-password" role="tabpanel" aria-labelledby="change-password-tab">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <h5>Change Password</h5>
+                                            </div>
+                                            <div class="card-body">
+                                                
+                                                <form action="{{route('user.profile.store')}}" method="post">
+								                    @csrf
+                                                    
+                                                    
+                                                       @if(session('status')) 
+                                                         <div class="alert alert-success" role="alert">
+									                       {{session('status')}}
+								                         </div>
+								                        @elseif(session('error'))
+								                        <div class="alert alert-danger" role="alert">
+									                        {{session('error')}}
+								                        </div>
+								
+								                        @endif
+                                                    
+                                                    
+                                                    <div class="row">
+                                                        
+                                                        <div class="form-group col-md-12">
+                                                            <label>Old Password <span class="required">*</span></label>
+                                                             <input type="password" name="old_password" class="form-control
+												                @error('old_password') is-invalid @enderror" id="old_password" />
+                                                                @error('old_password') 
+                                                                  <span class="text-danger">{{$message}}</span>
+												                @enderror
+                                                        </div>
+                                                        <div class="form-group col-md-12">
+                                                            <label>New Password <span class="required">*</span></label>
+                                                             <input type="password" name="new_password" class="form-control
+												                @error('new_password') is-invalid @enderror" id="new_password" />
+                                                                @error('new_password') 
+                                                                  <span class="text-danger">{{$message}}</span>
+												                @enderror
+                                                        </div>
+                                                        <div class="form-group col-md-12">
+                                                            <label>Confirm New Password <span class="required">*</span></label>
+                                                             <input type="password" name="new_password_confirmation" class="form-control" id="new_password_confirmation" />
+                                                               
+                                                        </div>
+                                                        
                                                         
                                                         <div class="col-md-12">
                                                             <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit" value="Submit">Save Change</button>
