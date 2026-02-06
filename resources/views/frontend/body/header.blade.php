888d6ca6c303b7c3c837d76efceb28490ae66863
kevin <kevinkalenga10@gmail.com>
2026-02-06 15:46:54 +0100

    Update User Login Setup With and Without Login

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -156,11 +156,13 @@
                                     <a href="{{route('register')}}">
                                         <img class="svgInject" alt="Nest" src="{{asset('frontend/assets/imgs/theme/icons/icon-user.svg')}}" />
                                     </a>
-                                    <a href="{{route('register')}}"><span class="lable ml-0">Account</span></a>
+                                    
+                                  @auth
+                                     <a href="#"><span class="lable ml-0">Account</span></a> 
                                     <div class="cart-dropdown-wrap cart-dropdown-hm2 account-dropdown">
                                         <ul>
                                             <li>
-                                                <a href="page-account.html"><i class="fi fi-rs-user mr-10"></i>My Account</a>
+                                                <a href="{{route('dashboard')}}"><i class="fi fi-rs-user mr-10"></i>My Account</a>
                                             </li>
                                             <li>
                                                 <a href="page-account.html"><i class="fi fi-rs-location-alt mr-10"></i>Order Tracking</a>
@@ -175,10 +177,15 @@
                                                 <a href="page-account.html"><i class="fi fi-rs-settings-sliders mr-10"></i>Setting</a>
                                             </li>
                                             <li>
-                                                <a href="page-login.html"><i class="fi fi-rs-sign-out mr-10"></i>Sign out</a>
+                                                <a href="{{route('user.logout')}}"><i class="fi fi-rs-sign-out mr-10"></i>Sign out</a>
                                             </li>
                                         </ul>
                                     </div>
+                                  @else
+                                     <a href="{{route('login')}}"><span class="lable ml-0">Login</span></a> 
+                                     <span class="lable" style="margin-left: 2px; margin-right:2PX;"> | </span>
+                                     <a href="{{route('register')}}"><span class="lable ml-0">Register</span></a>
+                                  @endauth
                                 </div>
                             </div>
                         </div>
