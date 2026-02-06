4e588a7ce9e7750c43f55ab57623cdc9cec7ceb9
kevin <kevinkalenga10@gmail.com>
2026-02-06 12:39:45 +0100

    User Profile Design Part3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,37 @@ class UserController extends Controller
         $userData = User::find($id);
          return view('index', compact('userData'));
     }
+
+    public function UserProfileStore(Request $request)
+    {
+        // get the authenticated user id
+       $id = Auth::user()->id;
+       $data = User::find($id);
+       $data->username = $request->username;
+       $data->name = $request->name;
+       $data->email = $request->email;
+       $data->phone = $request->phone;
+       $data->address = $request->address;
+
+       if($request->file('photo')) {
+          $file = $request->file('photo');
+        //   to remove an existing img before downloading
+          @unlink(public_path('upload/user_images/'.$data->photo));
+          $filename = date('YmdHi').$file->getClientOriginalName();
+          $file->move(public_path('upload/user_images'), $filename);
+        // insert in db and field name is photo  
+          $data['photo'] = $filename;
+       }
+
+       $data->save();
+
+        $notification = array(
+           'message' => 'User Profile Updated Successfully',
+           'alert-type' => 'success'
+        );
+
+
+       return redirect()->back()->with($notification);
+     
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -44,6 +44,8 @@
                                         <div class="card">
                                             <div class="card-header">
                                                 <h3 class="mb-0">Hello {{Auth::user()->name}}</h3>
+                                                <br>
+                                                <img  id="showImage"  src="{{(!empty($userData->photo)) ? url('upload/user_images/'.$userData->photo) : url('upload/default_avatar.jpg')}}" alt="User" class="rounded-circle p-1 bg-primary" width="110">
                                             </div>
                                             <div class="card-body">
                                                 <p>
@@ -165,7 +167,8 @@
                                             </div>
                                             <div class="card-body">
                                                 
-                                                <form method="post" name="enq">
+                                                <form action="{{route('user.profile.store')}}" method="post" enctype="multipart/form-data">
+								                    @csrf
                                                     <div class="row">
                                                         <div class="form-group col-md-6">
                                                             <label>User Name <span class="required">*</span></label>
